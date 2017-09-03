dc4121376299032b1a6f615479b8ee56eec2fedb
sree <sreelakshmias97@gmail.com>
2017-09-03 15:42:28 +0530

    Add home page and create users table

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+
+var models  = require(path.join(__dirname, '/../' ,'models'));
+var users = models.users;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { msg: '' });
+});
+
+router.post('/login', function(req, res, next) {
+	// users.findOne()
 });
 
 module.exports = router;
