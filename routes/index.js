3c343a445abf5d26ea0d0d5cd8acf05f61bc0c87
sree <sreelakshmias97@gmail.com>
2017-09-15 19:37:25 +0530

    Creation of table

--- routes/index.js
+++ routes/index.js
@@ -21,6 +21,19 @@ router.use(bodyParser.urlencoded({ extended: false }));
 router.get('/', function(req, res, next) {
 	router.host = req.protocol+'://'+req.get('host');
 	console.log(router.host);
+	// var userDetails = {
+ //  		aadhaarid: 222,
+ //  		name: "",
+ //  		dob: "",
+ //  		address: "",
+ //  		email: "",
+ //  		fppath: "",
+ //  		allowvote: "n",
+ //  		voted: "n"
+	// };
+	// users.sync({force: true}).then(function(){
+	// 	return users.create(userDetails);
+	// });
   	res.render('index', { msg: '' });
 });
 
@@ -56,7 +69,7 @@ router.get('/storeOrCheckDetails',passwordless.restricted({failureRedirect : '/'
 
 router.post('/login',passwordless.requestToken(
 	function(user, delivery, callback) {
-		users.findOne ({where : {email : user}})
+		users.findOne ({where: {email : user}})
 		.then(function(voter) {
 			if ( !voter ) {
 				callback(null,null);
@@ -84,4 +97,3 @@ router.get('/register', function(req,res,next) {
 });
 
 module.exports = router;
- 
